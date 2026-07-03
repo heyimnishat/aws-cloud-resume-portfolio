330474079971015a17739c343d7725e9a99897f7
Mohammad Nishat <im.mdnishat@gmail.com>
2026-07-03 23:17:24 +0530

    Created AWS project documentation structure

--- js/main.js
+++ js/main.js
@@ -20,3 +20,16 @@ document.querySelectorAll('.nav-links a').forEach(link => {
     });
 
 });
+
+async function getVisitorCount() {
+    try {
+        const response = await fetch("https://j8wje4q1n7.execute-api.us-east-1.amazonaws.com/prod/visitorcount");
+        const data = await response.json();
+
+        document.getElementById("visitor-count").textContent = data.count;
+    } catch (error) {
+        console.error("Visitor Counter Error:", error);
+    }
+}
+
+getVisitorCount();
